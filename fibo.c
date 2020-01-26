dc6a93e2185dd3e534a7e4f02fd1175242c1d77a
Shivam saini <noddycheeks@gmail.com>
2020-01-26 23:25:16 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -2,7 +2,7 @@ int main() {
     int n,temp, a = 0, b = 1;
     printf("%d\n", a);
     printf("%d\n", b);
-    scanf_s("%d", &n);
+    scanf("%d", &n);
     for (int i = 0; i < n-2; i++) {
         temp = b;
         b = a + b;

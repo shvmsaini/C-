f1d9f4ec593b473bc08d5eed3ebbfa81afceee1c
Shivam saini <noddycheeks@gmail.com>
2020-01-30 23:56:56 +0530

    Create 1incrementininteger.c

---
+++ 1incrementininteger.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() {
+    int n,i=1;
+    scanf("%d", &n);
+    int temp = n;
+    while (n > 0) {
+        int a = n % 10;
+        if (a == 9) {
+            temp = temp - 9 * i;
+            i *= 10;
+            n = n / 10;
+            continue;
+        }
+        else {
+            a++;
+        }
+        temp = temp + 1 * i;
+        i *= 10;
+        n = n / 10;
+            
+    }
+    printf("%d", temp);
+    
+    
+    return 0;
+}

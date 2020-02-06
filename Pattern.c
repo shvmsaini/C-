ad977b05ef5dd5195a40b8c7f96b319da7aea33e
Shivam saini <noddycheeks@gmail.com>
2020-02-06 18:27:32 +0530

    Pattern.c
    To print this pattern : https://imgur.com/zhC4Y56

--- Pattern.c
+++ Pattern.c
@@ -1,1 +1,29 @@
+#include<stdio.h> 
+int main()
+{
+    int rowl = 72, spl = -1, d = 70;
+    for (int cr = 0; cr < 7; cr++) {
+        for (int b = 65; b < rowl; b++) {
+            printf("%c", char(b));
+        }
+        for (int spa = 0; spa < spl; spa++) {
+            printf(" ");
+        }
+        
+        if (cr == 0) {
+            printf("FEDCBA\n");
+            rowl -= 1;
+            spl += 2;
+            continue;
+        }
+        for (int c = d; c > 64; c--) {
+            printf("%c", char(c));
+        }
+        rowl -= 1;
+        spl += 2;
+        d -= 1;
+        printf("\n");
 
+    }
+    return 0;
+}

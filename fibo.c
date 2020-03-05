973f5be7760ab7e4556897a823586da65bea679a
Shivam saini <noddycheeks@gmail.com>
2020-03-05 05:44:17 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -1,14 +1,26 @@
+#include <stdio.h>
+
+
 int main() {
-    int n,temp, a = 0, b = 1;
-    printf("%d\n", a);
-    printf("%d\n", b);
-    scanf("%d", &n);
-    for (int i = 0; i < n-2; i++) {
+    int n, temp, a = 0, b = 1;
+    printf("Enter a number upto which to print fibonacci series: ");
+    scanf_s("%d", &n);
+    if (n < 0) {
+        printf("Not a valid input. Enter again: ");
+        scanf_s("%d", &n);
+    }
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    printf("%d ", a);
+    printf("%d ", b);
+    
+    for (int i = 0; i < n - 2; i++) {
         temp = b;
         b = a + b;
-        printf("%d\n", b);
+        printf("%d ", b);
         a = temp;
-        
+
     }
     return 0;
 }

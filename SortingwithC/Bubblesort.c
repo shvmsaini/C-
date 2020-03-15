e8cc2b82798e1ae6149a5016aa98ca6ca56e20b2
Shivam saini <noddycheeks@gmail.com>
2020-03-15 17:44:12 +0530

    Update Bubblesort.c

--- SortingwithC/Bubblesort.c
+++ SortingwithC/Bubblesort.c
@@ -8,7 +8,7 @@ void swap(int* item1, int* item2) {
 }
 void bubblesort(int arr[], int n) {
 	int flag;
-	for (int i = 0; i < n -1; i++) { //-1 as last index is not necessary 
+	for (int i = 0; i < n -1; i++) { 
 		flag = 0;
 		for (int j = 0; j < n - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {

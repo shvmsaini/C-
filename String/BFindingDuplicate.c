f63a421c60883ffa921763a8cfcd77e88dec4443
Shivam saini <noddycheeks@gmail.com>
2020-04-22 17:12:23 +0530

    Update BFindingDuplicate.c

--- String/BFindingDuplicate.c
+++ String/BFindingDuplicate.c
@@ -5,13 +5,11 @@ int main()
     scanf_s("%s", str,20);
     int H = 0, x = 0,i;
     for (i = 0; str[i] != '\0'; i++) {
-        x = 1;
+        x = 1; 
+        /* if (str[i] - 97 < 0) continue; */ //add this condition to not to avoid cases
         x = x << str[i] - 97;
-
-
         if ((x & H) > 0) {
             printf("%c is duplicate ", str[i]);
-
         }
         else
             H = x | H;

9aca926c4717f832ae10a9a8ec58ce831216c2df
Shivam saini <noddycheeks@gmail.com>
2020-04-22 16:37:37 +0530

    Finding duplicate in string using bit manipulation
    This method also ignores cases

---
+++ String/BFindingDuplicate.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+int main()
+{
+    char str[20];
+    scanf_s("%s", str,20);
+    int H = 0, x = 0,i;
+    for (i = 0; str[i] != '\0'; i++) {
+        x = 1;
+        x = x << str[i] - 97;
+
+
+        if ((x & H) > 0) {
+            printf("%c is duplicate ", str[i]);
+
+        }
+        else
+            H = x | H;
+    }
+
+    return 0;
+}

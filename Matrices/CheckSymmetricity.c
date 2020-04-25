7b245edbec7cb0ebe7f87d6a66a90a6d81c6bc8e
Shivam saini <noddycheeks@gmail.com>
2020-04-25 12:07:03 +0530

    to check if a matrix is symmetric

---
+++ Matrices/CheckSymmetricity.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int sym(int A[][5],int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (A[i][j] != A[j][i]){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+int main(){
+    int A[][5]{ {2,1,2,2,2},
+        {2,3,3,3,3},
+        {2,3,4,4,4},
+        {2,3,4,5,5},
+        {2,3,4,5,6}
+    };
+    (sym(A, 5) == 0) ? (printf("No")) : printf("Yes");
+    
+
+    return 0;
+}

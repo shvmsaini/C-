d1923808c9bacadff8320877197d6bfdad6fdd40
Shivam saini <noddycheeks@gmail.com>
2020-04-28 23:59:44 +0530

    passed arrays using pointer

--- Matrices/CheckAnyToeplitz.c
+++ Matrices/CheckAnyToeplitz.c
@@ -1,52 +1,66 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+int toeplitz(int *A, int Row, int Col) {
 
-int toeplitz(int A[][6],int Row,int Col) {
-	
 	int y = 0;
-	int key;
-	int size = sizeof(A[0])/sizeof(int);
-	for (int i = 0; i < size-1;i++) { // upper half
-		key = A[0][i];
+	int key,size;
+	if (Row > Col) {
+		size = Row;
+	}
+	else {
+		size = Col;
+	}
+	
+	printf("%d\n", size);
+	for (int i = 0; i < size - 1; i++) { // upper half
+		key = *(A+i);
 		printf("Key = %d ***** ", key);
-		for (int j = 0; j < Row && j+y < Col; j++) {
-			if (key != A[j][j+y]) {
+		for (int j = 0; j < Row && j + y < Col; j++) {
+			if (key != A[(j*Col)+j+y]) {
 				return 0;
 			}
 		}
 		printf("upper check\n");
-		key = A[i][0];
+		key = *(A + i*Col);
 		printf("Key = %d ***** ", key);
-		for (int j = 0; j+y < Row && j  < Col; j++) {
-			if (key != A[j+y][j]) {
+		for (int j = 0; j + y < Row && j < Col; j++) {
+			if (key != A[(j+y) * Col + j]) {
 				return 0;
 			}
 		}
 		printf("lower check\n");
 		y++;
 	}
-	return 1;	
+	return 1;
 }
-
-int main() {	
-	/* int A[][5] = {
+//void foo(int* mat, int r, int c) {
+//	for (int i = 0; i < r; i++)
+//		for (int j = 0; j < c; j++)
+//			printf("%d ", *(mat + i * c + j));
+//}
+int main() {
+	int p[][5] = { { 1,2,3,4,5 },{ 6,7,8,9,10} };
+	int A[][5] = {
 		{2,3,4,5,6},
 		{7,2,3,4,5},
 		{8,7,2,3,4},
 		{9,8,7,2,3},
-		{0,9,8,7,2},
-		{1,0,9,8,7}
-	}; */
-	int B[][6]={
-		{1,3,4,5,6,7},
+		{8,9,8,7,2},
+		{1,8,9,8,7}
+	}; 
+	int B[][6] = {
+		{2,3,4,5,6,7},
 		{7,2,3,4,5,6},
 		{8,7,2,3,4,5},
 		{9,8,7,2,3,4},
 		{2,9,8,7,2,3},
 	};
+
+	toeplitz(A[0], 6, 5) == 0 ? (printf("No\n")) : printf("Yes\n");
+	toeplitz(B[0], 5, 6) == 0 ? (printf("No\n")) : printf("Yes\n");
 	
 	
-	/*toeplitz(A,6,5)==0? (printf("No")) : printf("Yes");*/
-	toeplitz(B,5,6)==0?(printf("No")) : printf("Yes");
+	
 	
 	return 0;
 }

c1cfaf78ed0e2d878aa7c8e3f61c07821e588c89
Shivam saini <noddycheeks@gmail.com>
2020-05-03 23:40:34 +0530

    Check if an LL is sorted

---
+++ LinkedList/CheckSort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+}*first = NULL, * second = NULL, * third = NULL;
+void Display(struct Node* p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+void create(int A[], int n)
+{
+    int i;
+    struct Node* t, * last;
+    first = (struct Node*)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node*)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+int isSorted(struct Node* p)
+{
+    int x = -65536;
+
+    while (p != NULL)
+    {
+        if (p->data < x)
+            return 0;
+        x = p->data;
+        p = p->next;
+    }
+    return 1;
+
+}
+int main()
+{
+   int A[] = { 10,20,30,40,50 };
+   create(A, 5);
+   isSorted(first)==1?printf("Yes, It is sorted."): printf("No, It is not sorted.");
+   return 0;
+}

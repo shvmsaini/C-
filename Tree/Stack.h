07f45b6031076c331d7e6a0fb0d9f787258da6d2
Shivam saini <noddycheeks@gmail.com>
2020-05-12 13:40:46 +0530

    Create Stack.h

---
+++ Tree/Stack.h
@@ -0,0 +1,53 @@
+#include "Queue.h"
+#ifndef Stack_h
+#define Stack_h
+#endif 
+
+struct Stack
+{
+    int size;
+    int top;
+    struct Node ** S;
+};
+void Stackcreate(struct Stack* st,int size)
+{
+   
+    st->size = size;
+    st->top = -1;
+    st->S = (struct Node **)malloc(st->size * sizeof(struct Node *));
+}
+
+void push(struct Stack* st, struct Node *x)
+{
+    if (st->top == st->size - 1)
+        printf("Stack overflow\n");
+    else
+    {
+        st->top++;
+        st->S[st->top] = x;
+    }
+
+}
+struct Node *pop(struct Stack* st)
+{
+    struct Node* x = NULL;
+
+    if (st->top == -1)
+        printf("Stack Underflow\n");
+    else
+    {
+        x = st->S[st->top--];
+    }
+    return x;
+}
+
+int isEmptyStack(struct Stack st)
+{
+    if (st.top == -1)
+        return 1;
+    return 0;
+}
+int isFull(struct Stack st)
+{
+    return st.top == st.size - 1;
+}
